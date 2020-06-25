832f17d6480d92fae3185c04a1e81edf99b7e4bb
braunbearded <braunbearded1@gmail.com>
2020-06-26 00:42:53 +0200

    better default font + font for color emoji

--- config.def.h
+++ config.def.h
@@ -6,16 +6,17 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "Hack Nerd Font:size=16:antialias=true:autohint=true",
+    "JoyPixels:size=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+    /*     fg         bg       */
+    [SchemeNorm] = { "#bbbbbb", "#222222" },
+    [SchemeSel] = { "#eeeeee", "#005577" },
     [SchemeSelHighlight] = { "#ffc978", "#005577" },
     [SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
