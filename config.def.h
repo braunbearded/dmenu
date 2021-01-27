75aab526a582b71ace9cab1b61c0732c661f6aeb
braunbearded <braunbearded1@gmail.com>
2021-01-27 01:01:44 +0100

    one theme + center

--- config.def.h
+++ config.def.h
@@ -2,24 +2,24 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "Hack Nerd Font:size=16:antialias=true:autohint=true",
+    "Hack Nerd Font:size=15:antialias=true:autohint=true",
     "JoyPixels:size=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = { "#bbbbbb", "#222222" },
-    [SchemeSel] = { "#eeeeee", "#005577" },
-    [SchemeSelHighlight] = { "#ffc978", "#005577" },
-    [SchemeNormHighlight] = { "#ffc978", "#222222" },
-    [SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeNorm] = { "#abb2bf", "#282c34" },
+    [SchemeSel] = { "#5c6370", "#61afef" },
+    [SchemeSelHighlight] = { "#e06c75", "#61afef" },
+    [SchemeNormHighlight] = { "#e06c75", "#282c34" },
+    [SchemeOut] = { "#131613", "#56b6c2" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 20;
 static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
 
 /*
@@ -29,4 +29,4 @@ static unsigned int lineheight = 0;         /* -h option; minimum height of a me
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 1;
